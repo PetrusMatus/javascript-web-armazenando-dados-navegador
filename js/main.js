983db81e82b5afaf9f834c4ec96f8d43b602d834
e-pmatos <e-pmatos@tap.pt>
2023-03-31 16:02:53 +0100

    createElement | classList.add | innerHTML | appendChild

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
 const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    console.log(e);
+    //console.log(e);
     //console.log(e.target[0].value);
     //todos os elementos do formulário
-    console.log(e.target.elements);
-    console.log(e.target.elements['nome']);
-    console.log(e.target.elements['quantidade']);
-})
+    //console.log(e.target.elements);
+    //console.log(e.target.elements['nome'].value);
+    //console.log(e.target.elements['quantidade'].value);
+    const nome = e.target.elements['nome'].value;
+    const quantidade = e.target.elements['quantidade'].value;
+    criaElemento(nome,quantidade);
+
+});
+
+
+function criaElemento(nome, quantidade){
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item');
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+
+    //console.log(numeroItem);
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    //console.log(novoItem);
+    lista.appendChild(novoItem);
+}
+
+
