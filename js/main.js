244563fae55d8b35adb800e638e19ef9530fe52d
e-pmatos <e-pmatos@tap.pt>
2023-03-31 17:22:37 +0100

    criar elementos forEach item

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ console.log(JSON.parse(localStorage.getItem('itens')));
 itens.forEach( (element) => {
     console.log(element.nome);
     console.log(element.quantidade);
+    //criaElemento(element.nome,element.quantidade);
+    criaElemento(element);
 })
 
 
@@ -22,46 +24,45 @@ form.addEventListener('submit', (e) => {
     //console.log(e.target.elements['quantidade'].value);
     const nome = e.target.elements['nome'];
     const quantidade = e.target.elements['quantidade'];
-    criaElemento(nome.value,quantidade.value);
-
+    
+    
+    const itemAtual = atualizaLocalStorage(nome.value,quantidade.value);
+    //criaElemento(nome.value,quantidade.value);
+    criaElemento(itemAtual);
     nome.value = '';
     quantidade.value = '';
 
 });
 
 
-function criaElemento(nome, quantidade){
+function criaElemento(item){
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = item.quantidade;
 
     //console.log(numeroItem);
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += item.nome;
 
     //console.log(novoItem);
     lista.appendChild(novoItem);
 
     //localStorage.setItem('nome' , nome);
     //localStorage.setItem('quantidade' , quantidade);
+}
 
+
+function atualizaLocalStorage(nome, quantidade){
     const itemAtual = {
         nome: nome,
         quantidade: quantidade
     }
     
-
     itens.push(itemAtual);
-
     localStorage.setItem('itens' , JSON.stringify(itens));
-   
 
-    
-
-
+    return itemAtual;
 }
-
-
